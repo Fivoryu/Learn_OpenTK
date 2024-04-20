1d50f7c5f18b7994d94aae0716c90ec4b4059ba1
Fivoryu <trevorfelixcalerosuyo@gmail.com>
2024-04-20 19:39:57 -0400

    Rabbit

--- obj/Debug/net8.0/Hello_OpenTK.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hello_OpenTK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello_OpenTK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2117b546c893b88d46e44963c9e7893d3cb20119")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+805b2cd31beb643e5adafad7545c19d1cfa7129d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello_OpenTK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello_OpenTK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
